b7af1b3b79630b74d4affaba5c22968c9c396628
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-09-07 20:33:38 -0300

    Created a manifest

---
+++ contexts/TaskContext.js
@@ -0,0 +1,29 @@
+'use client'
+
+import { createContext, useContext, useState } from "react"
+
+// Create context
+const TaskContext = createContext()
+
+// Hook or function to access context
+export const useTaskContext = () => {
+    return useContext(TaskContext)
+}
+
+// Context provider
+export const TaskProvider = ({children}) => {
+    const [tasks, setTasks] = useState([]);
+
+    const addNewTask = (task) => {
+        setTasks = ((prevTasks) => [...prevTasks, task])
+    }
+
+    return (
+        <TaskContext.Provider value = {{tasks, addNewTask}}>
+            {children}
+        </TaskContext.Provider>
+    )
+        
+    
+}
+
